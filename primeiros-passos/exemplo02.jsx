3032a76ac2321fab6f939537f27786f51bc4e54c
Vinícius Nascimento <vini_sn@outlook.com>
2024-02-19 13:16:59 -0300

    Retornando mais de uma tag JSX

---
+++ primeiros-passos/exemplo02.jsx
@@ -0,0 +1,10 @@
+// Retornando mais de uma tag JSX num componente
+
+function AboutPage() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Hello there.<br />How do you do?</p>
+        </>
+    )
+}
