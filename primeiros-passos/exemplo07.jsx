a9225e8d9e1d4b59701e5fba905880505e0c7187
Vinícius Nascimento <vini_sn@outlook.com>
2024-02-21 20:20:08 -0300

    Respondendo a eventos

---
+++ primeiros-passos/exemplo07.jsx
@@ -0,0 +1,12 @@
+// RESPONDENDO A EVENTOS
+function MyButton() {
+    function handleClick() {
+        alert('You clicked me!');
+    }
+
+    return (
+        <button onClick={handleClick}>
+            Click me
+        </button>
+    );
+}
