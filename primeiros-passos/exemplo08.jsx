f4efa78de47de9d036b215ce70861cbd8bbb8394
Vinícius Nascimento <vini_sn@outlook.com>
2024-02-22 14:15:58 -0300

    Exibindo número de cliques num botão

---
+++ primeiros-passos/exemplo08.jsx
@@ -0,0 +1,29 @@
+// UPDATING THE SCREEN
+
+import { useState } from "react";
+
+// Componente MyButton
+function MyButton() {
+    // Variável de estado
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    return (
+        <button onClick={handleClick}>
+            Clicked {count} times
+        </button>
+    );
+}
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Counters that update separately</h1>
+            <MyButton />
+            <MyButton />
+        </div>
+    )
+}
