692635f85828b4cc41fa7e098d003e862600d67c
Vinícius Nascimento <vini_sn@outlook.com>
2024-03-27 13:53:15 -0300

    Components: ProductCategoryRow and ProductRow

---
+++ thinking-in-react/App.jsx
@@ -0,0 +1,23 @@
+function ProductCategoryRow({ category }) {
+    return (
+        <tr>
+            <th colSpan="2">
+                {category}
+            </th>
+        </tr>
+    );
+}
+
+function ProductRow({product}) {
+    const name = product.stocked?product.name:
+    <span style={{color:'red'}}>
+        {product.name}
+    </span>;
+
+    return(
+        <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
+    );
+}
