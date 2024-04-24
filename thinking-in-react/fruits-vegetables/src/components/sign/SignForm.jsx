8eb58fa6c7a2df6dae49f0b2d65d3466d0341866
Vinícius Nascimento <vini_sn@outlook.com>
2024-04-24 14:10:08 -0300

    Sign form finished

--- thinking-in-react/fruits-vegetables/src/components/sign/SignForm.jsx
+++ thinking-in-react/fruits-vegetables/src/components/sign/SignForm.jsx
@@ -8,20 +8,31 @@ export default function SignForm() {
             <div className="d-flex container justify-content-center">
                 <form action="#" className="form-width p-3 border rounded shadow-sm" method="post">
                     <div className="mb-1">
-                        <label htmlFor="user" className="form-label mb-1">Digite seu usuário:</label>
-                        <input type="text" className="form-control input-focus-none" placeholder="Usuário" id="user" />
+                        <label htmlFor="name" className="form-label mb-1">Digite seu nome:</label>
+                        <input type="text" className="form-control input-focus-none" placeholder="Nome" id="name" />
                     </div>
 
-                    <div className="mb-2">
-                        <div><label htmlFor="pass" className="form-label mb-1">Digite sua senha:</label></div>
+                    <div className="mb-1">
+                        <label htmlFor="email" className="form-label mb-1">Digite seu email:</label>
+                        <input type="email" className="form-control input-focus-none" placeholder="E-mail" id="email" />
+                    </div>
+
+                    <div className="mb-1">
+                        <label htmlFor="pass" className="form-label mb-1">Digite sua senha:</label>
                         <input type="password" className="form-control input-focus-none" placeholder="Senha" id="pass" />
                     </div>
 
+                    <div className="mb-2">
+                        <div><label htmlFor="passconfirm" className="form-label mb-1">Confirme sua senha:</label></div>
+                        <input type="password" className="form-control input-focus-none" placeholder="Confirmação" id="passconfirm" />
+                    </div>
+
                     <div className="d-flex justify-content-between">
-                        <div className="align-self-center">
+                        <div className="align-self-center">                            
+                            <p className="m-0">Já possui cadastro?</p>
                             <a href="/login">
-                                <p className="text-primary m-0">Login</p>
-                            </a>
+                                <p className="m-0">Faça seu Login!</p>
+                            </a>                            
                         </div>
                         <div className="align-self-center"><input type="submit" className="btn btn-primary" value="Enviar" /></div>
                     </div>
